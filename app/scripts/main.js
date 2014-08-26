8f0626040dac74413410a1c6a1c4705f9bbb35d0
Scott Leitch <s.d.leitch@gmail.com>
2014-08-25 21:23:53 -0400

    added stand-in Skills section, changed main.js so #navbar dropdown disappears again when link is clicked. Having trouble getting link anchors working.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,8 +5,13 @@
 
     $('#nav-toggle').on('click', function(eventObject) {
       eventObject.preventDefault();
-      $(this).toggleClass('active');
+      // $(this).toggleClass('active');
       $('#navbar').toggleClass('active');
+    });
+
+    $('.list-link').on('click', function(eventObject) {
+      eventObject.preventDefault();
+      $('#navbar').removeClass('active');
     });
 
   });
