4a217d6d95ebe988c80d71df6ecfd6feb4a59631
Scott Leitch <s.d.leitch@gmail.com>
2014-10-07 21:06:53 -0400

    made a lot of changes. Will comment in GitHub web client

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,15 +3,65 @@
 (function($) {
   $(document).ready(function() {
 
+    // Toggle navbar on hamburger button click
     $('#nav-toggle').on('click', function(eventObject) {
       eventObject.preventDefault();
-      // $(this).toggleClass('active');
+      $(this).toggleClass('active');
       $('#navbar').toggleClass('active');
     });
 
+    //When navbar link clicked, untoggle 
     $('.list-link').on('click', function(eventObject) {
       eventObject.preventDefault();
-      $('#navbar').removeClass('active');
+      $('#nav-toggle').toggleClass('active');
+      $('#navbar').toggleClass('active');
+    });
+
+    //Skills stretchbar code - stretch/shrink when clicked.
+    $('.col').click(function() {
+      //If mobile window, stretch .col tall
+      if ($(window).width() < 800) {
+        if ($(this).hasClass('col1')) {
+          $('#skills').css('top', '100vh');
+        }
+        else if ($(this).hasClass('col2')) {
+          $('#skills').css('top', '75vh');
+        }
+        else if ($(this).hasClass('col3')) {
+          $('#skills').css('top', '50vh');
+        }
+        else if ($(this).hasClass('col4')) {
+          $('#skills').css('top', '25vh');
+        }
+      }
+      //if wider window, stretch .col wide.
+      else {
+        if ($(this).hasClass('col1')) {
+          $('#skills').css('left', '0%');
+        }
+        else if ($(this).hasClass('col2')) {
+          $('#skills').css('left', '-25%');
+        }
+        else if ($(this).hasClass('col3')) {
+          $('#skills').css('left', '-50%');
+        }
+        else if ($(this).hasClass('col4')) {
+          $('#skills').css('left', '-75%');
+        }
+      }
+
+      //if .col active, remove. If inactive, make active.
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $('#skills').css('top', '100vh');
+        $('#skills').css('left', '0px');
+        // $('#nav-toggle').removeClass('active');
+      }
+      else {
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+        // $('#nav-toggle').addClass('active');
+      }
     });
   });
 })(jQuery);
